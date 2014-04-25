e10b60d0b9043a7cb19a6f793e18acd1dd68b980
Josh Justice <josh@need-bee.com>
2014-04-25 13:59:18 -0400

    fixes a bug where, when label not set, html code is shown as the placeholder

--- src/NpmWeb/FormBuilder/FormBuilder.php
+++ src/NpmWeb/FormBuilder/FormBuilder.php
@@ -48,7 +48,7 @@ class FormBuilder
 			// default: do nothing
 		}
 
-		$config = $this->_processOptions($name, $options);
+		$config = $this->_processOptions($fieldname, $options);
 		return $this->_outputHelper( $fieldname, $config, $value );
 	}
 
@@ -177,7 +177,7 @@ class FormBuilder
 			: [];
 
 		if (!array_key_exists('id', $config->extras) ) $config->extras['id'] = esc_attr( $name );
-		if (!array_key_exists('placeholder',$config->extras) ) $config->extras['placeholder'] = $config->label; // configurable?
+		if (!array_key_exists('placeholder',$config->extras) ) $config->extras['placeholder'] = self::formatLabel($name,null); // configurable?
 
 		$config->errors = array_key_exists('errors',$options)
 			? $options['errors']
