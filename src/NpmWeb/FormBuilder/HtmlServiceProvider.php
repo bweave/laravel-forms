36cb076b2ddd42c893d3e8370b2008edd67991d9
Josh Justice <josh@need-bee.com>
2014-04-30 12:17:31 -0400

    changes base class to use foundation formatting

--- src/NpmWeb/FormBuilder/HtmlServiceProvider.php
+++ src/NpmWeb/FormBuilder/HtmlServiceProvider.php
@@ -1,7 +1,7 @@
 <?php namespace NpmWeb\FormBuilder;
 
 class HtmlServiceProvider 
-	extends \NpmWeb\ClientValidationGenerator\Laravel\HtmlServiceProvider
+	extends \NpmWeb\ClientValidationGenerator\Laravel\FoundationHtmlServiceProvider
 {
 
 	protected function createFormBuilder($app) {
