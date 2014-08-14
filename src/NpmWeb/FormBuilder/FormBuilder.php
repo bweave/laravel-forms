86f1cf6956bf0c3d0452accefcbffe8b0a71cf80
Josh Justice <josh@need-bee.com>
2014-08-14 14:32:33 -0400

    removes framework-specific code in parent class

--- src/NpmWeb/FormBuilder/FormBuilder.php
+++ src/NpmWeb/FormBuilder/FormBuilder.php
@@ -420,31 +420,6 @@ class FormBuilder
         return $config;
 
     }
-/*
-
-Sample Foundation Text Input
-<div class="large-6 columns">
-    <label for="contact_first_name">First Name
-        <input type="text" name="contact_first_name" id="contact_first_name" maxlength="50" placeholder="First Name" required/>
-        <small class="error">First Name is required</small>
-    </label>
-</div><!--/ large-6 -->
-
-Sample prefixed text input
-<div class="medium-6 columns">
-    <label for="pmt_amount">Gift Amount (USD)
-        <div class="row collapse">
-            <div class="small-3 large-2 columns">
-                <span class="prefix">$</span>
-            </div>
-            <div class="small-9 large-10 columns">
-                <input type="text" name="pmt_amount" id="pmt_amount" maxlength="14" placeholder="0.00" required>
-                <small class="error">Please Specify Amount</small>
-            </div><!--/ small-9 -->
-        </div><!--/ row -->
-    </label>
-</div>
-*/
 
     protected function _outputHelper( $fieldname, $config, $control ) {
         $error = null;
@@ -455,82 +430,6 @@ Sample prefixed text input
         }
 
         return $this->renderer->renderFormControl($fieldname, $config, $error, $control);
-    }
-
-    protected function _outputHelperFoundation( $fieldname, $config, $control ) {
-
-        //Log::debug(__METHOD__.'('.$fieldname.')');
-        $error = null;
-        if( property_exists($config,'errors')
-            && $config->errors instanceof \Illuminate\Support\MessageBag
-        ) {
-            $error = $config->errors->first($fieldname);
-        }
-        $rowPerField = false;
-        if( property_exists($config,'row_per_field') ) {
-            $rowPerField = $config->row_per_field;
-        }
-        ob_start();
-        ?>
-        <?php if( $rowPerField ): ?><div class="row"><?php endif ?>
-        <div class="<?php echo $config->columns_class ?> columns">
-            <label for="<?php echo $config->extras['id'] ?>" <?php if($error) { echo 'class="error"'; } ?>><?php echo $config->label; ?>
-            <?php if ( isset($config->prefix) ) { ?>
-            <div class="row collapse <?php if($error) { echo 'error'; } ?>">
-                <div class="<?php echo $config->prefix['columns_class'] ?> columns">
-                    <span class="prefix"><?php echo $config->prefix['label']; ?></span>
-                </div>
-                <div class="<?php echo $config->main['columns_class'] ?> columns"><?php } // end if prefix ?>
-                <?php echo $control /* pre-escaped */ ?>
-                <?php if($error): ?>
-                    <small class="error"><?php echo $error ? esc_body($error) : '' ?></small>
-                <?php endif ?>
-            <?php if ( isset($config->prefix) ) { ?>
-                </div>
-            </div><?php } ?>
-            </label>
-        </div>
-        <?php if( $rowPerField ): ?></div><?php endif ?>
-        <?php
-        return ob_get_clean();
-    }
-
-    protected function _outputHelperBootstrap( $fieldname, $config, $control ) {
-
-        //Log::debug(__METHOD__.'('.$fieldname.')');
-        $error = null;
-        if( property_exists($config,'errors')
-            && $config->errors instanceof \Illuminate\Support\MessageBag
-        ) {
-            $error = $config->errors->first($fieldname);
-        }
-        $rowPerField = false;
-        if( property_exists($config,'row_per_field') ) {
-            $rowPerField = $config->row_per_field;
-        }
-        ob_start();
-        ?>
-        <?php if( $rowPerField ): ?><div class="row"><?php endif ?>
-            <div class="form-group <?php echo $config->columns_class ?><?php if($error) { echo ' has-error'; } ?>">
-                <label for="<?php echo $config->extras['id'] ?>"><?php echo $config->label; ?></label>
-                <?php if ( isset($config->prefix) ) { ?>
-                <div class="row collapse <?php if($error) { echo 'error'; } ?>">
-                    <div class="<?php echo $config->prefix['columns_class'] ?> columns">
-                        <span class="prefix"><?php echo $config->prefix['label']; ?></span>
-                    </div>
-                    <div class="<?php echo $config->main['columns_class'] ?> columns">
-                <?php } // end if prefix ?>
-                <?php echo $control /* pre-escaped */ ?>
-                <?php if($error): ?>
-                    <span class="help-block"><?php echo $error ? esc_body($error) : '' ?></small>
-                <?php endif ?>
-                <?php if ( isset($config->prefix) ) { ?>
-                    </div>
-                </div><?php } ?>
-            </div>
-        <?php if( $rowPerField ): ?></div><?php endif ?>
-        <?php
-        return ob_get_clean();
     }
 
 }
