300fb8c4a299600b55a281ff9ce365a3fab1a428
Josh Justice <josh@need-bee.com>
2015-02-10 11:08:35 -0500

    changes tooltip icon to info

--- src/NpmWeb/FormBuilder/Renderers/FoundationBasicGridRenderer.php
+++ src/NpmWeb/FormBuilder/Renderers/FoundationBasicGridRenderer.php
@@ -18,7 +18,7 @@ class FoundationBasicGridRenderer implements RendererInterface {
             <div class="<?php echo e($config->columns_class) ?> columns">
                 <label for="<?php echo e($config->extras['id']) ?>" <?php if($error) { echo 'class="error"'; } ?>><?php echo e($config->label) ?>
                 <?php if ( isset($config->tooltip) ) { ?>
-                    <span style='margin-left: 4px;' class='has-tip tip-top fi-flag' data-tooltip aria-haspopup="true" title="<?php echo e($config->tooltip) ?>"></span>
+                    <span style='margin-left: 4px;' class='has-tip tip-top fi-info' data-tooltip aria-haspopup="true" title="<?php echo e($config->tooltip) ?>"></span>
                 <?php } ?>
                 <?php if ( isset($config->prefix) ) { ?>
                     <div class="row collapse <?php if($error) { echo 'error'; } ?>">
